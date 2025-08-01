46bf5ac267de64caeea96c832728b32c9139d0a4
Asshley <asshleydeanwaynegozar@gmail.com>
2025-08-01 16:15:29 +0800

    add more mock account details data for rendering more data

--- utils/accounts.ts
+++ utils/accounts.ts
@@ -1,33 +1,98 @@
 const accounts = [
 	{
-        id: 1,
+		id: 1,
 		balance: 2334,
 		accountName: 'GOTyme',
 		accountCategory: 'Bank',
 	},
 	{
-        id: 2,
+		id: 2,
 		balance: 1928,
 		accountName: 'Cash',
 		accountCategory: 'Cash',
 	},
 	{
-        id: 3,
+		id: 3,
 		balance: 2928,
 		accountName: 'SeaBank',
 		accountCategory: 'Bank',
 	},
 	{
-        id: 4,
+		id: 4,
 		balance: 2837,
 		accountName: 'BPI',
 		accountCategory: 'Bank',
-	},{
-        id: 5,
-        balance: 9399,
-        accountName: 'BDO',
-        accountCategory: 'Bank'
-    }
+	},
+	{
+		id: 5,
+		balance: 9399,
+		accountName: 'BDO',
+		accountCategory: 'Bank',
+	},
+	{
+		id: 6,
+		balance: 68362,
+		accountName: 'Maya',
+		accountCategory: 'E-Wallet',
+	},
+
+	{
+		id: 7,
+		balance: 68362,
+		accountName: 'Maya',
+		accountCategory: 'E-Wallet',
+	},
+
+	{
+		id: 8,
+		balance: 68362,
+		accountName: 'Maya',
+		accountCategory: 'E-Wallet',
+	},
+
+	{
+		id: 9,
+		balance: 68362,
+		accountName: 'Maya',
+		accountCategory: 'E-Wallet',
+	},
+
+	{
+		id: 10,
+		balance: 68362,
+		accountName: 'Maya',
+		accountCategory: 'E-Wallet',
+	},
+	{
+		id: 11,
+		balance: 68362,
+		accountName: 'Maya',
+		accountCategory: 'E-Wallet',
+	},
+	{
+		id: 12,
+		balance: 68362,
+		accountName: 'Maya',
+		accountCategory: 'E-Wallet',
+	},
+	{
+		id: 13,
+		balance: 68362,
+		accountName: 'Maya',
+		accountCategory: 'E-Wallet',
+	},
+	{
+		id: 14,
+		balance: 68362,
+		accountName: 'Maya',
+		accountCategory: 'E-Wallet',
+	},
+	{
+		id: 15,
+		balance: 68362,
+		accountName: 'Maya',
+		accountCategory: 'E-Wallet',
+	},
 ];
 
 export default accounts;
