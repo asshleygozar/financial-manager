72d08251c95cd92971deaf132761d1b722f97e3a
Asshley <asshleydeanwaynegozar@gmail.com>
2025-08-01 15:29:53 +0800

    add active class condition for setting active classname

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -2,9 +2,11 @@
 import styles from '@/styles/components/left-side-bar.module.css';
 import { NavigationButton } from './ui/navbutton';
 import { useStorage } from '@/context/useStorage';
+import { usePathname } from 'next/navigation';
 import { Home, Account, Investment, Settings } from './ui/icons';
 
 function LeftSidebar() {
+	const pathname = usePathname();
 	const { handleClick, setClick } = useStorage();
 	return (
 		<aside className={styles.container}>
@@ -18,21 +20,25 @@ function LeftSidebar() {
 			</button>
 
 			<NavigationButton
+				className={pathname === '/home' ? styles.active : ''}
 				icon={<Home />}
 				linkName='Home'
 				path='/home'
 			/>
 			<NavigationButton
+				className={pathname === '/accounts' ? styles.active : ''}
 				icon={<Account />}
 				linkName='Accounts'
 				path='/accounts'
 			/>
 			<NavigationButton
+				className={pathname === '/investments' ? styles.active : ''}
 				icon={<Investment />}
 				linkName='Investments'
 				path='/investments'
 			/>
 			<NavigationButton
+				className={pathname === '/settings' ? styles.active : ''}
 				icon={<Settings />}
 				linkName='Settings'
 				path='/settings'
