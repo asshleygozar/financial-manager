6f7f1c34e725274ec931e0aa9e4f7457eee5db4d
Asshley <asshleydeanwaynegozar@gmail.com>
2025-08-01 12:21:59 +0800

    remove class names for migration from tailwind to css module

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,36 +1,36 @@
+import styles from '@/styles/components/ui/card.module.css';
 type Cards = {
 	title: string;
 	balance: number;
 	currency: string;
 	date?: string;
-	style?: string;
-    headColor?: string;
+	headColor?: string;
 	accountName?: string;
 	accountCategory?: string;
 };
 
-function MainCard({ style, title, balance, currency, date }: Cards) {
+function MainCard({ title, balance, currency, date }: Cards) {
 	return (
-		<div className={style}>
-			<h1 className='font-outfit text-[3rem] text-purple-600 justify-self-center'>{title}</h1>
-			<h2 className='text-[3rem]'>
-				<span className="text-yellow-400">{currency} </span>
+		<div className={`${styles.mainCard} ${styles.card}`}>
+			<h1>{title}</h1>
+			<h2>
+				<span>{currency} </span>
 				{balance}
 			</h2>
-			<h3 className=''>As of {date}</h3>
+			<h3>As of {date}</h3>
 		</div>
 	);
 }
 
-function SubCard({ headColor, title, balance, currency, date }: Cards) {
+function SubCard({ title, balance, currency, date }: Cards) {
 	return (
-		<div className='bg-(--card-background) rounded-lg flex flex-col justify-between p-[1rem]'>
-			<h1 className={headColor}>{title}</h1>
-			<h2 className='text-[1.7rem]'>
+		<div className={`${styles.subCard} ${styles.card}`}>
+			<h1>{title}</h1>
+			<h2>
 				<span>{currency}</span>
 				{balance}
 			</h2>
-			<h3 className='justify-self-end self-end text-[0.8rem]'>
+			<h3>
 				As of <span>{date}</span>
 			</h3>
 		</div>
