125c572af3cc3140585eed68d48d203f3cc5621b
Asshley <asshleydeanwaynegozar@gmail.com>
2025-08-06 22:25:57 +0800

    initialize create account modal

---
+++ components/CreateAccountModal.tsx
@@ -0,0 +1,17 @@
+import styles from '@/styles/create-account.module.css';
+import { AccountCard } from './ui/card';
+
+function CreateAccountModal() {
+    return (
+        <div className={styles.container}>
+            <AccountCard
+                balance={0.00}
+                accountName='Cash'
+                accountCategory='Cash'
+
+            />
+        </div>
+    );
+}
+
+export default CreateAccountModal;
