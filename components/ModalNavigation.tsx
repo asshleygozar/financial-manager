09b41a99e8ff79addee590a945a5fb3b0f72efd5
Asshley <asshleydeanwaynegozar@gmail.com>
2025-08-12 12:49:11 +0800

    add modal nav button for navigation

--- components/ModalNavigation.tsx
+++ components/ModalNavigation.tsx
@@ -1,16 +1,36 @@
 'use client';
+import styles from '@/styles/components/modal-navigation.module.css';
+import ModalNavigationButton from './ui/ModalNavButton';
 import { useNavigation } from '@/context/useNavigation';
+import { Account, Home } from './ui/icons';
 
 const ModalNavigation = () => {
-	const { setNavigation } = useNavigation();
+	const { navigation, setNavigation } = useNavigation();
 	return (
-		<nav>
-			<ul>
-				<li onClick={() => setNavigation('new-transaction')}>
-					New transaction
-				</li>
-				<li onClick={() => setNavigation('create-account')}>Create account</li>
-			</ul>
+		<nav className={styles.container}>
+			<h1>Navigation</h1>
+			<ModalNavigationButton
+				className={
+					navigation === 'new-transaction'
+						? `${styles.button} ${styles.active}`
+						: `${styles.button}`
+				}
+				icon={<Home />}
+				name='New transaction'
+				path='new-transaction'
+				setNavigation={setNavigation}
+			/>
+			<ModalNavigationButton
+				className={
+					navigation === 'create-account'
+						? `${styles.button} ${styles.active}`
+						: `${styles.button}`
+				}
+				icon={<Account />}
+				name='Create Account'
+				path='create-account'
+				setNavigation={setNavigation}
+			/>
 		</nav>
 	);
 };
